58e397c4fe95d8207ce9ae633129c309435d0e58
cddaaaa1 <yuduan334@gmail.com>
2026-09-10 15:04:38 +0800

    working on 在NVBoard上实现双控开关

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+// #include "Vtop.h"
+// #include "verilated.h"
 
-int main() {
-  printf("Hello, ysyx!\n");
+int main(int argc, char** argv) {
+  Verilated::commandArgs(argc, argv);
+  Vtop* top = new Vtop;
+
+  while (!Verilated::gotFinish()) {
+    int a = rand() & 1;
+    int b = rand() & 1;
+    top->a = a;
+    top->b = b;
+    top->eval();
+    printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+    assert(top->f == (a ^ b));
+  }
+
+  top->final(); 
+  delete top;
   return 0;
 }
